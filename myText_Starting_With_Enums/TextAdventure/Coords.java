20cdf14e28e187fec2cab6305d6e858ee93240e8
MTrecozzi <47111522+MTrecozzi@users.noreply.github.com>
2019-04-08 11:07:22 -0400

    Coordinates System

---
+++ myText_Starting_With_Enums/TextAdventure/Coords.java
@@ -0,0 +1,34 @@
+
+/**
+ * Write a description of class Coords here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Coords
+{
+    // instance variables - replace the example below with your own
+    private int x;
+    private int y;
+
+    /**
+     * Constructor for objects of class Coords
+     */
+    public Coords(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
